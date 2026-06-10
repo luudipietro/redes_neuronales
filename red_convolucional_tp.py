8aea0abb784a2b79e7b3a158d3ff3930151b4450
Luciano Note <huesodipietro@gmail.com>
2026-06-10 16:32:46 -0300

    trabajo en clase

---
+++ red_convolucional_tp.py
@@ -0,0 +1,18 @@
+
+"""
+Tp Final
+El objetivo es armar una CNN para resolver un problema de visión
+Problema de clasificación
+Existen problemas
+. Clasification
+. Object detection (bounding boxes)
+. Segmentation -- > Formas de cosas
+. T.T.I
+
+Minimo de 5 clases
+Modelo de visión custom
+Al menos dos modelos VGG 16 REGNET, competir con el modelo de vision (solo el modelo de vision)
+El kernel es solo la matriz, aplicar la convolucion
+El kernel no neceseriamente tiene que ser cuadrado
+
+"""
